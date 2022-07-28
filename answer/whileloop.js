a212cb0f03da88dbb40dbb721bf7d45e7622cc4c
Dr HAOXIANG GAO <61554320+GaoLAQ@users.noreply.github.com>
2022-07-28 22:06:57 +0100

    Update whileloop.js

--- answer/whileloop.js
+++ answer/whileloop.js
@@ -1,1 +1,20 @@
+let sum = 0 
 
+for(let i = 0; i<5; i++){
+   sum += i 
+}
+
+let i = 0 
+while(i<5){
+  sum += i 
+  i++ 
+}
+console.log(sum)
+
+
+let j = 0 
+while(ture){
+  sum += j 
+  if(j>4) break 
+}
+console.log(sum) 
